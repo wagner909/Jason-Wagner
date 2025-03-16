c68be9fc84118aad103ee60361db923bf6fd7ec2
wagner909 <wagner909@hotmail.com>
2025-03-16 11:14:31 -0400

    Add files via upload

---
+++ Lab2/js/hello.js
@@ -0,0 +1,19 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+  greeting = 'Good evening';
+} else if (hourNow > 12) {
+  greeting = 'Good afternoon';
+} else if (hourNow > 0) {
+  greeting = 'Good morning';
+} else {
+  greeting = 'Welcome';
+}
+
+document.write(greeting);
+document.write("<br>");
+
+var currentTime = today.getHours() + ":" + today.getMinutes();
+document.write("Current time: " + currentTime);
